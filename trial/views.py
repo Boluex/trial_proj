96e50058bd0cabfc4072f9f48328c2bc2971b90c
Boluex <awomewekolawole1@gmail.com>
2024-01-29 17:11:01 +0100

    Added another views

--- trial/views.py
+++ trial/views.py
@@ -6,4 +6,8 @@ from django.http import HttpResponse
 # Create your views here.
 
 def home(request):
-   return HttpResponse('it worked')
+   return HttpResponse('it worked ')
+   
+   
+def response(request):
+   return HttpResponse('it is working ')
